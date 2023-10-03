83dc58a5284652fbc0a266d5eab516f9ca0e86b9
Alex Younger <thealexyounger@proton.me>
2023-10-03 12:21:50 -0400

    feat: add options to bionicify markdown function
    This change adds options where users can choose to use either astericks
    or underscores for the bionification of the markdown.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import remarkStringify from "remark-stringify";
 
 import mdToBionic from "./local-plugins/md-to-bionic.js";
 
-function bionicifyMarkdown(md) {
+function bionicifyMarkdown(md, options) {
   const vFile = unified()
     .use(remarkParse)
     .use(mdToBionic)
@@ -13,10 +13,28 @@ function bionicifyMarkdown(md) {
     .use(remarkFrontmatter)
     .processSync(md);
 
-  const markdownContent = vFile.value
-    .replace(/\\\_/g, "_")
-    .replace(/\\\[/g, "[");
-  return markdownContent;
+  // It was originally defaulting to use underscores, but some parsers have a problem with this.
+  // Adding both underscores and asterisks regex as a workaround.
+  // The default is to use the normal textVide package, which uses underscores.
+  if (options && Object.keys(options).length) {
+    const useUnderscore = options?.useUnderscore ?? false;
+    const useAsterick = options?.useAsterick ?? false;
+
+    if (useUnderscore) {
+      const markdownContent = vFile.value
+        .replace(/\\\_/g, "_")
+        .replace(/\\\[/g, "[");
+      return markdownContent;
+    }
+
+    if (useAsterick) {
+      const markdownContent = vFile.value
+        .replace(/\\\_/g, "*")
+        .replace(/\\\[/g, "[");
+      return markdownContent;
+    }
+  }
+  return vFile.value
 }
 
 export default bionicifyMarkdown;
